ace3322f4026dce221cb15aa44c524760d931051
denysbespalenko <denis.bespalenko@gmail.com>
2020-10-16 01:58:13 +0300

    finish project config

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,33 @@
 const path = require('path');
-const {CleanWebpackPlugin} = require('clean-webpack-plugin'); //delete ./dist files while project rebuild
-const HTMLWebpackPlugin = require('html-webpack-plugin'); //create HTML template
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const isProd = process.env.NODE_ENV === 'production';
+const isDev = !isProd;
+const filename = ext => isDev ? `bundle.${ext}` : `bundle.[hash].${ext}`;
+const jsLoaders = () => {
+    const loaders = [
+        {
+            loader: 'babel-loader',
+            options: {
+                presets: ['@babel/preset-env'],
+            },
+        },
+    ];
+    if (isDev) {
+        loaders.push('eslint-loader');
+    }
+    return loaders;
+};
 
 module.exports = {
     context: path.resolve(__dirname, 'src'),
     mode: 'development',
-    entry: './index.js',
+    entry: ['@babel/polyfill', './index.js'],
     output: {
-        filename: 'bundle.[hash].js',
-        path: path.resolve(__dirname, 'dist')
+        filename: filename('js'),
+        path: path.resolve(__dirname, 'dist'),
     },
     resolve: {
         extensions: ['.js'],
@@ -19,21 +36,30 @@ module.exports = {
             '@core': path.resolve(__dirname, 'src/core'),
         },
     },
+    devtool: isDev ? 'source-map' : false,
+    devServer: {
+        port: 3000,
+        hot: isDev,
+    },
     plugins: [
         new CleanWebpackPlugin(),
         new HTMLWebpackPlugin({
-            template: 'index.html'
+            template: 'index.html',
+            minify: {
+                removeComments: isProd,
+                collapseWhitespace: isProd,
+            },
         }),
         new CopyPlugin({
             patterns: [
                 {
                     from: path.resolve(__dirname, 'src/favicon.ico'),
-                    to: path.resolve(__dirname, 'dist')
+                    to: path.resolve(__dirname, 'dist'),
                 },
             ],
         }),
         new MiniCssExtractPlugin({
-            filename: 'bundle.[hash].css',
+            filename: filename('css'),
         }),
     ],
     module: {
@@ -49,12 +75,7 @@ module.exports = {
             {
                 test: /\.m?js$/,
                 exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env']
-                    },
-                },
+                use: jsLoaders(),
             },
         ],
     },
